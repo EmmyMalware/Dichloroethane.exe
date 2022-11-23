bda7ee2ccbd1b93df6bafa5608f6d4f9bd6e1033
Emmy-malware <114366661+Emmys-malwares@users.noreply.github.com>
2022-11-23 15:29:34 -0600

    code

---
+++ Dichloroethane/Dichloroethane/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Dichloroethane
+{
+    public class Program
+    {
+        static void Main() {}
+    }
+    public partial class Message : Form
+    {
+        public Message()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Run Malware?", "Dichloroethane.exe", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+                 MessageBox.Show("Are you sure? It overwrite the mbr so ur pc wont boot.");
+        }
+    }
+}
